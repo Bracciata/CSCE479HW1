94d64e72535f593a24041076f0995404e3162152
Thomas Braccia <bracciata@gmail.com>
2022-02-15 20:22:41 -0600

    Added a train function

--- model_CIFAR.py
+++ model_CIFAR.py
@@ -30,3 +30,37 @@ class ModelOne:
             # data is uint8 by default, so we have to cast it
             self.conv_classifier(tf.cast(batch['image'], tf.float32))
             break
+
+    def train(self, ds):
+        optimizer = tf.keras.optimizers.Adam()
+
+        loss_values = []
+        accuracy_values = []
+        # Loop through one epoch of data
+        for epoch in range(1):
+            for batch in tqdm(ds):
+                with tf.GradientTape() as tape:
+                    # run network
+                    x = tf.reshape(
+                        tf.cast(batch['image'], tf.float32), [-1, 784])
+                    labels = batch['label']
+                    logits = self.conv_classifier(x)
+
+                    # calculate loss
+                    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+                        logits=logits, labels=labels)
+                loss_values.append(loss)
+
+                # gradient update
+                grads = tape.gradient(
+                    loss, self.conv_classifier.trainable_variables)
+                optimizer.apply_gradients(
+                    zip(grads, self.conv_classifier.trainable_variables))
+
+                # calculate accuracy
+                predictions = tf.argmax(logits, axis=1)
+                accuracy = tf.reduce_mean(
+                    tf.cast(tf.equal(predictions, labels), tf.float32))
+                accuracy_values.append(accuracy)
+
+        print(self.conv_classifier.summary())
