09addcc949669e5163a113a04d70c740225e4055
Thomas Braccia <bracciata@gmail.com>
2022-02-19 20:42:26 -0600

    New Main from Sarat

--- main-FMNIST.py
+++ main-FMNIST.py
@@ -8,11 +8,117 @@ import pandas as pd
 from tqdm import tqdm
 
 
+DATA_DIR = './tensorflow-datasets/'
+
+
+ds = tfds.load('fashion_mnist', data_dir=DATA_DIR, shuffle_files=True)
+train_ds = ds['train'].shuffle(1024).batch(16)
+
+
+train = tfds.load('fashion_mnist', split='train[:90%]', data_dir=DATA_DIR)
+validation = tfds.load(
+    'fashion_mnist', split='train[-10%:]', data_dir=DATA_DIR)
+
+val_ds = ds['train'].shuffle(1024).batch(16)
+
+class_names = ["T-shirts", "Trousers", "Pullovers",
+               "Dress", "Coat", "Sandal", "Shirt", "Bag", "Ankle boot"]
+
+
+for batch in train_ds:
+    print("data shape:", batch['image'].shape)
+    print("data shape:", batch['image'].dtype)
+    labl = batch['label']
+    print("label:", labl)
+    break
+
+
+test = batch['image'][5000:]
+# visualize some of the data
+
+idx = np.random.randint(batch['label'].shape[0])
+idx1 = np.random.randint(batch['image'].shape[0])
+idx2 = np.random.randint(batch['image'].shape)
+idx3 = np.random.randint(batch['image'].shape[2])
+
+
+print("idx is: ", idx)
+#print("idx2 is: ", idx2)
+#print("idx3 is: ", idx3)
+
+#print("batch image: ", batch['image'][idx])
+
+#print("batch image is: ", batch['image'][idx])
+
+print("An image looks like this:")
+imgplot = plt.imshow(batch['image'][idx])
+#imgplot2 = plt.imshow(batch['image'][idx3])
+
+
+print("It's colored because matplotlib wants to provide more contrast than just greys")
+
+print("label number is: ", labl[idx])
+
+
+class Dense(tf.Module):
+    def __init__(self, output_size, activation=tf.nn.relu):
+
+        super(Dense, self).__init__(name=name)
+        self.output_size = output_size
+        self.activation = activation
+        self.is_built = False
+
+    def _build(self, x):
+        data_size = x.shape[-1]
+        self.W = tf.Variable(tf.random.normal(
+            [data_size, self.output_size]), name='weights')
+        self.b = tf.Variable(tf.random.normal([self.output_size]), name='bias')
+        self.is_built = True
+
+    def __call__(self, x):
+        if not self.is_built:
+            self._build(x)
+        return self.activation(tf.matmul(x, self.W) + self.b)
+
+
+L2_COEFF = 0.1
+
+
+class L2DenseNetwork(tf.Module):
+    def __init__(self, name=None):
+        super(L2DenseNetwork, self).__init__(name=name)
+        self.dense_layer1 = tf.keras.layers.Dense(200, activation=tf.nn.relu)
+        self.dense_layer2 = tf.keras.layers.Dense(10)
+        self.is_built = False
+
+    def _build(self, x):
+        self.is_built = True
+
+    def l2_loss(self):
+        return tf.nn.l2_loss(self.dense_layer1.kernel) + tf.nn.l2_loss(self.dense_layer2.kernel)
+
+    @tf.function
+    def __call__(self, x):
+        if not self.is_built:
+            self._build(x)
+        embed = self.dense_layer1(x)
+        output = self.dense_layer2(embed)
+        #print("output loss: ", output)
+        return output
+
+
+if 'l2_dense_net' not in locals():
+    l2_dense_net = L2DenseNetwork()
+l2_dense_net(tf.ones([1, 100]))
+
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=[28, 28]))
 model.add(tf.keras.layers.Dense(90, tf.nn.relu))
 model.add(tf.keras.layers.Dense(50, tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+
+#model.compile(optimizer="adam", loss="sparse_softmax_crossentropy", metrics=["accuracy"])
+#history = model.fit(train,epochs=30,validation_data=(validation))
 
 optimizer = tf.keras.optimizers.Adam()
 
@@ -97,3 +203,5 @@ print("Accuracy:", np.mean(accuracy_values))
 loss_values = np.concatenate(loss_values)
 plt.hist(loss_values, density=True, bins=30)
 plt.show()
+
+#print("it's a ", class_names[batch['label'].shape[0]]);
